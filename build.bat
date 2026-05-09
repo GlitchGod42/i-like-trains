f43bf50a95a14f2904866cfdeba8b7ded5e51d1d
GlitchGod42 <tobikmotyka54@gmail.com>
2026-05-09 14:17:02 +0200

    downgrade to 1.21.11

---
+++ build.bat
@@ -0,0 +1,3 @@
+cmd /C "gradlew build"
+cd "C:/Users/PC/Documents/Github/i-like-trains/build/libs"
+copy /Y "*.jar" "C:\Users\PC\AppData\Roaming\PrismLauncher\instances\Fabulously Optimized\minecraft\mods"
