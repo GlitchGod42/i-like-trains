3af4308a4b71e8c05639eda19a3361d022ce0970
GlitchGod42 <tobikmotyka54@gmail.com>
2026-05-09 14:19:45 +0200

    Delete build.bat

--- build.bat
+++
@@ -1,3 +0,0 @@
-cmd /C "gradlew build"
-cd "C:/Users/PC/Documents/Github/i-like-trains/build/libs"
-copy /Y "*.jar" "C:\Users\PC\AppData\Roaming\PrismLauncher\instances\Fabulously Optimized\minecraft\mods"
